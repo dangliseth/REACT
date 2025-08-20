111defeaa7f582777b56f8388a9d53ac57815d15
dangliseth <danglisr@student.apc.edu.ph>
2025-08-20 19:25:52 +0800

    separated square function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
-export default function Square() {
-  return <button className="square">X</button>;
+function Square({value}) {
+  function handleClick() {
+    console.log('Clicked!')
+  }
+  return <button className="square"
+          onClick={handleClick}>
+            {value}
+        </button>
+}
+
+export default function Board() {
+  return (
+  <>
+    <div className="board-row">
+      <Square value="1" />
+      <Square value="1" />
+      <Square value="1" />
+    </div>
+    <div className="board-row">
+      <Square value="1" />
+      <Square value="1" />
+      <Square value="1" />
+    </div>
+    <div className="board-row">
+      <Square value="1" />
+      <Square value="11" />
+      <Square value="1" />
+    </div>
+  </>
+  );
 }
